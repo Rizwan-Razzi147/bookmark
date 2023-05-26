7f662562fc45829e96e29a6f667f54f4b8c5ca07
Rizwan <rizwandevops147@gmail.com>
2023-05-26 16:50:19 +0500

    bookmark application javaScript

---
+++ book.js
@@ -0,0 +1,4 @@
+const urlinput = document.querySelector(".url")
+const titleinput = document.querySelector(".title")
+const btn = document.querySelector('.bookmark-btn');
+const list = document.querySelector('.output-list');
